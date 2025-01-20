9cf919e6a84eb143435b6f128ce582403abb1e0d
Hiimax <shodekebarry@gmail.com>
2025-01-20 09:14:15 +0100

    line 1226

--- ios/Classes/InAppWebView/InAppWebView.swift
+++ ios/Classes/InAppWebView/InAppWebView.swift
@@ -1223,8 +1223,8 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate, WKNavi
         }
     }
 
-    public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: ((Any?, Error?) -> Void)? = nil) {
-        if let applePayAPIEnabled = options?.applePayAPIEnabled, applePayAPIEnabled {
+    public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: (@MainActor @Sendable (Any?, (any Error)?) -> Void)? = nil) {
+        if let applePayAPIEnabled = settings?.applePayAPIEnabled, applePayAPIEnabled {
             if let completionHandler = completionHandler {
                 completionHandler(nil, nil)
             }
